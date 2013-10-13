fbe316d3da8304f2f81fbc8cc794a9fa89acee5f
Jon Seidel <jseidel@edpci.com>
2013-10-12 21:44:52 -0700

    Added sub-grid retrieval by number position

--- lib/sudoku_board.rb
+++ lib/sudoku_board.rb
@@ -5,8 +5,8 @@ class SudokuBoard
   def initialize(game_file)
     @board = []
     File.readlines(game_file).each do |line|
-     row = convert_line_to_row(line)
-     @board << row unless row.nil?
+      row = convert_line_to_row(line)
+      @board << row unless row.nil?
     end
     @board
   end
@@ -16,19 +16,47 @@ class SudokuBoard
   def row(row)
     @board[row-1]
   end
-  def row= (row)
-  end
   def col(col)
     column = []
     @board.each { |row| column << row[col-1] }
     column
   end
+  def sub_grid(number)
+    # SubGrids are numbered 1-9 like this:
+    #   1,2,3
+    #   4,5,6
+    #   7,8.9
+    row, col = sub_grid_start(number)
+    s_grid = []
+    3.times do
+      3.times do
+        s_grid << self[row,col]
+        col += 1
+      end
+      row += 1
+      col -= 3
+    end
+    s_grid
+  end
   def convert_line_to_row(line)
     row = []
     line.chomp!.delete!('-+| ')
     return nil if line.empty?
-    line.tr!('[1-9].','[1-9]0')
+    line.tr!('.','0')
     line.each_char.map { |c| row << c.to_i }
     row
+  end
+  def sub_grid_start(number)
+    row = case
+          when number <=3 then 1
+          when number <=6 then 4
+          when number <=9 then 7
+          end
+    col = case
+          when number %3 == 1 then 1
+          when number %3 == 2 then 4
+          when number %3 == 3 then 7
+          end
+    [row,col]
   end
 end
--- spec/sudoku_validator_spec.rb
+++ spec/sudoku_validator_spec.rb
@@ -10,16 +10,24 @@ describe SudokuValidator do
     before do
       @game = SudokuBoard.new("valid_complete.sudoku")
     end
-    it "reads the first character of the sudoku file" do
+    it "returns the first character of the sudoku file" do
       expect(@game[1,1]).to eql 8
     end
-    it "reads the first row of the sudoku file" do
+    it "returns an arbitrary element from the board" do
+      expect(@game[4,5]).to eql 4
+    end
+    it "returns the first row of the sudoku file" do
       expect(@game.row(1)).to eql [8,5,9,6,1,2,4,3,7]
     end
-    it "reads the first column of the sudoku file" do
+    it "returns the first column of the sudoku file" do
       expect(@game.col(1)).to eql [8,7,1,9,3,2,4,6,5]
     end
-  
+    it "returns a 3x3 sub-grid as an array" do
+      expect(@game.sub_grid(1)).to eql [8,5,9,7,2,3,1,6,4]
+    end
+    it "returns a middle 3x3 sub-grid" do
+      expect(@game.sub_grid(5)).to eql [1,4,7,2,6,8,5,9,3]
+    end
   end
 
 end
