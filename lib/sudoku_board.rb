741dee444c488e5fa7dd62e064fb238272dbf293
Jon Seidel <jseidel@edpci.com>
2013-10-12 20:48:59 -0700

    Create Sudoku board class & test...
    New class reads input file and creates game board;
    Methods to return a specific element, row, or column.

--- lib/sudoku_board.rb
+++ lib/sudoku_board.rb
@@ -1,8 +1,34 @@
 class SudokuBoard
+  #attr_accessor :row
+  #attr_reader :col, []
+
   def initialize(game_file)
-    @board = [[8]]
+    @board = []
+    File.readlines(game_file).each do |line|
+     row = convert_line_to_row(line)
+     @board << row unless row.nil?
+    end
+    @board
   end
   def [] (row, col)
     @board[row-1][col-1]
+  end
+  def row(row)
+    @board[row-1]
+  end
+  def row= (row)
+  end
+  def col(col)
+    column = []
+    @board.each { |row| column << row[col-1] }
+    column
+  end
+  def convert_line_to_row(line)
+    row = []
+    line.chomp!.delete!('-+| ')
+    return nil if line.empty?
+    line.tr!('[1-9].','[1-9]0')
+    line.each_char.map { |c| row << c.to_i }
+    row
   end
 end
--- spec/sudoku_validator_spec.rb
+++ spec/sudoku_validator_spec.rb
@@ -8,11 +8,16 @@ describe SudokuValidator do
   describe SudokuBoard do
 
     before do
-      
+      @game = SudokuBoard.new("valid_complete.sudoku")
     end
     it "reads the first character of the sudoku file" do
-      board = SudokuBoard.new ("../valid_complete.sudoku")
-      expect(board[1,1]).to eql 8
+      expect(@game[1,1]).to eql 8
+    end
+    it "reads the first row of the sudoku file" do
+      expect(@game.row(1)).to eql [8,5,9,6,1,2,4,3,7]
+    end
+    it "reads the first column of the sudoku file" do
+      expect(@game.col(1)).to eql [8,7,1,9,3,2,4,6,5]
     end
   
   end
