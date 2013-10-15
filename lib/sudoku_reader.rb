3c32a28967aa1eaf4c4a890c63da0c8428ece32a
GCorbel <guirec.corbel@gmail.com>
2013-10-15 18:15:56 -0400

    refactoring

--- lib/sudoku_reader.rb
+++ lib/sudoku_reader.rb
@@ -1,16 +1,26 @@
 class SudokuReader
-  attr_accessor :lines
+  attr_reader :path
 
   def initialize(path)
-    @lines = []
-    File.read(path).each_line do |line|
-      unless separator?(line)
-        lines << line.split(' ').map { |value| cleaned_value(value) }
-      end
-    end
+    @path = path
+  end
+
+  def grid
+    fill_grid_from_file unless @grid
+    @grid
   end
 
   private
+  def fill_grid_from_file
+    @grid = []
+    File.read(path).each_line {|line| convert_line_to_values(line)}
+  end
+
+  def convert_line_to_values(line)
+    unless separator?(line)
+      @grid << line.split.map { |value| cleaned_value(value) }
+    end
+  end
 
   def separator?(line)
     line == "------+------+------\n"
--- spec/sudoku_reader_spec.rb
+++ spec/sudoku_reader_spec.rb
@@ -2,66 +2,90 @@ require_relative '../lib/sudoku_reader'
 
 describe SudokuReader do
   context "with the valid grid" do
-    it "give lines" do
-      reader = SudokuReader.new('valid_complete.sudoku')
-      grid = [[8,5,9,6,1,2,4,3,7],
-              [7,2,3,8,5,4,1,6,9],
-              [1,6,4,3,7,9,5,2,8],
-              [9,8,6,1,4,7,3,5,2],
-              [3,7,5,2,6,8,9,1,4],
-              [2,4,1,5,9,3,7,8,6],
-              [4,3,2,9,8,1,6,7,5],
-              [6,1,7,4,2,5,8,9,3],
-              [5,9,8,7,3,6,2,4,1]]
-      expect(reader.lines).to eq grid
+    it "give grid" do
+      expect(reader.grid).to eq grid
+    end
+
+    def reader
+      SudokuReader.new('valid_complete.sudoku')
+    end
+
+    def grid
+      [[8,5,9,6,1,2,4,3,7],
+       [7,2,3,8,5,4,1,6,9],
+       [1,6,4,3,7,9,5,2,8],
+       [9,8,6,1,4,7,3,5,2],
+       [3,7,5,2,6,8,9,1,4],
+       [2,4,1,5,9,3,7,8,6],
+       [4,3,2,9,8,1,6,7,5],
+       [6,1,7,4,2,5,8,9,3],
+       [5,9,8,7,3,6,2,4,1]]
     end
   end
 
   context "with the invalid complete grid" do
-    it "give lines" do
-      reader = SudokuReader.new('invalid_complete.sudoku')
-      grid = [[8,5,9,6,1,2,4,3,7],
-              [7,2,3,8,5,4,1,6,9],
-              [1,6,4,3,7,9,5,2,8],
-              [9,8,6,1,4,7,3,5,2],
-              [3,7,5,8,6,2,9,1,4],
-              [2,4,1,5,9,3,7,8,6],
-              [4,3,2,9,8,1,6,7,5],
-              [6,1,7,4,2,5,8,9,3],
-              [5,9,8,7,3,6,2,4,1]]
-      expect(reader.lines).to eq grid
+    it "give grid" do
+      expect(reader.grid).to eq grid
+    end
+
+    def reader
+      SudokuReader.new('invalid_complete.sudoku')
+    end
+
+    def grid
+      [[8,5,9,6,1,2,4,3,7],
+       [7,2,3,8,5,4,1,6,9],
+       [1,6,4,3,7,9,5,2,8],
+       [9,8,6,1,4,7,3,5,2],
+       [3,7,5,8,6,2,9,1,4],
+       [2,4,1,5,9,3,7,8,6],
+       [4,3,2,9,8,1,6,7,5],
+       [6,1,7,4,2,5,8,9,3],
+       [5,9,8,7,3,6,2,4,1]]
     end
   end
 
   context "with the valid incomplete grid" do
-    it "give lines" do
-      reader = SudokuReader.new('valid_incomplete.sudoku')
-      grid= [[8,5,0,0,0,2,4,0,0],
-             [7,2,0,0,0,0,0,0,9],
-             [0,0,4,0,0,0,0,0,0],
-             [0,0,0,1,0,7,0,0,2],
-             [3,0,5,0,0,0,9,0,0],
-             [0,4,0,0,0,0,0,0,0],
-             [0,0,0,0,8,0,0,7,0],
-             [0,1,7,0,0,0,0,0,0],
-             [0,0,0,0,3,6,0,4,0]]
-      expect(reader.lines).to eq grid
+    it "give grid" do
+      expect(reader.grid).to eq grid
+    end
+
+    def reader
+      SudokuReader.new('valid_incomplete.sudoku')
+    end
+
+    def grid
+      [[8,5,0,0,0,2,4,0,0],
+       [7,2,0,0,0,0,0,0,9],
+       [0,0,4,0,0,0,0,0,0],
+       [0,0,0,1,0,7,0,0,2],
+       [3,0,5,0,0,0,9,0,0],
+       [0,4,0,0,0,0,0,0,0],
+       [0,0,0,0,8,0,0,7,0],
+       [0,1,7,0,0,0,0,0,0],
+       [0,0,0,0,3,6,0,4,0]]
     end
   end
 
   context "with the invalid incomplete grid" do
-    it "give lines" do
-      reader = SudokuReader.new('invalid_incomplete.sudoku')
-      grid= [[8,5,0,0,0,2,4,0,0],
-             [7,2,0,0,8,0,0,0,9],
-             [0,0,4,0,0,0,0,0,0],
-             [0,0,0,1,0,7,0,0,2],
-             [3,0,5,0,0,0,9,0,0],
-             [0,4,0,0,0,0,0,0,0],
-             [0,5,0,0,8,0,0,7,0],
-             [0,1,7,0,0,0,0,0,0],
-             [0,0,0,0,3,6,0,4,0]]
-      expect(reader.lines).to eq grid
+    it "give grid" do
+      expect(reader.grid).to eq grid
+    end
+
+    def reader
+      SudokuReader.new('invalid_incomplete.sudoku')
+    end
+
+    def grid
+      [[8,5,0,0,0,2,4,0,0],
+       [7,2,0,0,8,0,0,0,9],
+       [0,0,4,0,0,0,0,0,0],
+       [0,0,0,1,0,7,0,0,2],
+       [3,0,5,0,0,0,9,0,0],
+       [0,4,0,0,0,0,0,0,0],
+       [0,5,0,0,8,0,0,7,0],
+       [0,1,7,0,0,0,0,0,0],
+       [0,0,0,0,3,6,0,4,0]]
     end
   end
 end
