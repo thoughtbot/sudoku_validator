6d7f65180bf63e8f564d482ddaa0e0225b06ba8f
GCorbel <guirec.corbel@gmail.com>
2013-10-15 17:56:21 -0400

    add test for last files

--- spec/sudoku_reader_spec.rb
+++ spec/sudoku_reader_spec.rb
@@ -32,4 +32,36 @@ describe SudokuReader do
       expect(reader.lines).to eq grid
     end
   end
+
+  context "with the valid incomplete grid" do
+    it "give lines" do
+      reader = SudokuReader.new('valid_incomplete.sudoku')
+      grid= [[8,5,0,0,0,2,4,0,0],
+             [7,2,0,0,0,0,0,0,9],
+             [0,0,4,0,0,0,0,0,0],
+             [0,0,0,1,0,7,0,0,2],
+             [3,0,5,0,0,0,9,0,0],
+             [0,4,0,0,0,0,0,0,0],
+             [0,0,0,0,8,0,0,7,0],
+             [0,1,7,0,0,0,0,0,0],
+             [0,0,0,0,3,6,0,4,0]]
+      expect(reader.lines).to eq grid
+    end
+  end
+
+  context "with the invalid incomplete grid" do
+    it "give lines" do
+      reader = SudokuReader.new('invalid_incomplete.sudoku')
+      grid= [[8,5,0,0,0,2,4,0,0],
+             [7,2,0,0,8,0,0,0,9],
+             [0,0,4,0,0,0,0,0,0],
+             [0,0,0,1,0,7,0,0,2],
+             [3,0,5,0,0,0,9,0,0],
+             [0,4,0,0,0,0,0,0,0],
+             [0,5,0,0,8,0,0,7,0],
+             [0,1,7,0,0,0,0,0,0],
+             [0,0,0,0,3,6,0,4,0]]
+      expect(reader.lines).to eq grid
+    end
+  end
 end
