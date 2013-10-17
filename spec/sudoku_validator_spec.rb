76dddb7b1b33c7828477330821401958a4d94180
GCorbel <guirec.corbel@gmail.com>
2013-10-17 17:58:32 -0400

    make the validator spec dryer

--- spec/sudoku_validator_spec.rb
+++ spec/sudoku_validator_spec.rb
@@ -2,132 +2,73 @@ require_relative '../lib/sudoku_validator'
 require_relative '../lib/sudoku_reader.rb'
 
 describe SudokuValidator do
-  before do
-    allow(reader).to receive(:reader).and_return(reader)
+  describe "#complete?" do
+    context "with complete sudokus" do
+      it "give true" do
+        ['valid_complete.sudoku', 'invalid_complete.sudoku'].each do |file|
+          expect(validator_for(file)).to be_complete
+        end
+      end
+    end
+
+    context "with incomplete sudokus" do
+      it "give false" do
+        ['valid_incomplete.sudoku', 'invalid_incomplete.sudoku'].each do |file|
+          expect(validator_for(file)).to_not be_complete
+        end
+      end
+    end
   end
 
-  shared_examples "a complete sudoku" do
-    describe "#complete?" do
+  describe "#valid?" do
+    context "with valid sudokus" do
+      it "give true" do
+        ['valid_complete.sudoku', 'valid_incomplete.sudoku'].each do |file|
+          expect(validator_for(file)).to be_valid
+        end
+      end
+    end
+
+    context "with invalid sudokus" do
+      it "give true" do
+        ['invalid_complete.sudoku',
+         'invalid_incomplete.sudoku',
+         'invalid_column_complete.sudoku',
+         'invalid_block_incomplete.sudoku',
+         'invalid_line_complete.sudoku'].each do |file|
+          expect(validator_for(file)).to_not be_valid
+        end
+      end
+    end
+  end
+
+  describe "#message" do
+    context "when the sudoku is valid" do
       context "when the sudoku is complete" do
-        it "is compete" do
-          expect(validator).to be_complete
+        it "say than the sudoku is valid" do
+          file = 'valid_complete.sudoku'
+          expect(validator_for(file).message).to eq "This sudoku is valid."
         end
       end
-    end
-  end
 
-  shared_examples "a incomplete sudoku" do
-    describe "#complete?" do
       context "when the sudoku is incomplete" do
-        it "is not complete" do
-          expect(validator).to_not be_complete
+        it "say than the sudoku is valid" do
+          file = 'valid_incomplete.sudoku'
+          expect(validator_for(file).message).to eq "This sudoku is valid, but incomplete."
+        end
+      end
+    end
+
+    context "when the sudoku is invalid" do
+      it "say than the sudoku is invalid" do
+        ['invalid_complete.sudoku', 'invalid_incomplete.sudoku'].each do |file|
+          expect(validator_for(file).message).to eq "This sudoku is invalid."
         end
       end
     end
   end
 
-  shared_examples "a valid sudoku" do
-    describe "#valid?" do
-      context "when the sudoku is valid" do
-        it "is valid" do
-          expect(validator).to be_valid
-        end
-      end
-    end
-  end
-
-  shared_examples "a invalid sudoku" do
-    describe "#valid?" do
-      context "when the sudoku is invalid" do
-        it "is not valid" do
-          expect(validator).to_not be_valid
-        end
-      end
-    end
-
-    describe "#message" do
-      it "say than the sudoku is valid" do
-        expect(validator.message).to eq "This sudoku is invalid."
-      end
-    end
-  end
-
-  describe "valid and complete sudoku" do
-    it_behaves_like "a complete sudoku"
-    it_behaves_like "a valid sudoku"
-
-    describe "#message" do
-      it "say than the sudoku is valid" do
-        expect(validator.message).to eq "This sudoku is valid."
-      end
-    end
-
-    def reader
-      SudokuReader.new('valid_complete.sudoku')
-    end
-  end
-
-  describe "valid an incomplete sudoku" do
-    it_behaves_like "a incomplete sudoku"
-    it_behaves_like "a valid sudoku"
-
-    describe "#message" do
-      it "say than the sudoku is valid" do
-        expect(validator.message).to eq "This sudoku is valid, but incomplete."
-      end
-    end
-
-    def reader
-      SudokuReader.new('valid_incomplete.sudoku')
-    end
-  end
-
-  describe "invalid an complete sudoku" do
-    it_behaves_like "a complete sudoku"
-    it_behaves_like "a invalid sudoku"
-
-    def reader
-      SudokuReader.new('invalid_complete.sudoku')
-    end
-  end
-
-  describe "invalid an incomplete sudoku" do
-    it_behaves_like "a incomplete sudoku"
-    it_behaves_like "a invalid sudoku"
-
-    def reader
-      SudokuReader.new('invalid_incomplete.sudoku')
-    end
-  end
-
-  describe "invalid lines and complete" do
-    it_behaves_like "a complete sudoku"
-    it_behaves_like "a invalid sudoku"
-
-    def reader
-      SudokuReader.new('invalid_complete.sudoku')
-    end
-  end
-
-  describe "invalid columns and complete" do
-    it_behaves_like "a complete sudoku"
-    it_behaves_like "a invalid sudoku"
-
-    def reader
-      SudokuReader.new('invalid_column_complete.sudoku')
-    end
-  end
-
-  describe "invalid subblock and incomplete" do
-    it_behaves_like "a incomplete sudoku"
-    it_behaves_like "a invalid sudoku"
-
-    def reader
-      SudokuReader.new('invalid_block_incomplete.sudoku')
-    end
-  end
-
-  def validator
-    @validator ||= SudokuValidator.new(reader)
+  def validator_for(file)
+    SudokuValidator.new(SudokuReader.new(file))
   end
 end
