7e21e61ef6637f005143244c5182835dacbc3ba2
Eric Lubin <elubin@elubin.com>
2013-10-24 09:55:41 -0400

    spec cleanup

--- lib/board_reader.rb
+++ lib/board_reader.rb
@@ -10,9 +10,14 @@ module Sudoku
       open(@file).each_with_index do |l,index|
         board << l.chomp!
         # could get more involved checking specific lines here like ---+---+---
-        raise "Invalid board string [#{board[index]}]" if board[index].match(/^[1-9|+.\-\s]+$/) == nil
+        raise "Invalid board string [#{board[index]}]" if board[index].match(valid_chars) == nil
       end
       board
+    end
+
+    private
+    def valid_chars
+      /^[1-9|+.\-\s]+$/
     end
   end
 end
