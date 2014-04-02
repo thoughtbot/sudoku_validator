12d6111045907af21791c536a14ff740b29499a9
nadegepepin <nadege.rique@gmail.com>
2014-04-02 03:32:56 +0000

    added << to simplify the grid creation

--- loader.rb
+++ loader.rb
@@ -4,16 +4,14 @@ require_relative 'sudoku'
 class Loader	
 
     def self.load(path)
-    	grid = Array.new(9){Array.new(9)}
+    	grid = Array.new()
     	File.open(path) do |f|
-        	i = 0
         	f.each_line do |line|
                 # removes the vertical separator | and replaces it with a space 
     			line = line.gsub('|', ' ') 
                 # the lines containing a + are horizontal separators, they are skipped
             	if !line.include? '+'   
-            		grid[i] = line.split(" ")
-            		i += 1
+            		grid << line.split(" ")	
             	end
           	end 	
         end
