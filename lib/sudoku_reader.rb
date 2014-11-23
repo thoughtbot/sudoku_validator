09bbfeafd44a9b54bf06503bb30cc5bc983e31d3
Austio@windowslaptop <lonnieastory@gmail.com>
2014-11-22 21:51:19 -0600

    Refactored to a Sedoku module namespace

--- lib/sudoku_reader.rb
+++ lib/sudoku_reader.rb
@@ -1,13 +1,49 @@
-class SudokuReader
-  def initialize sedoku
-    @sedoku = sedoku
+module Sedoku
+  class File
+    attr_reader :matrix, :file
+
+    def initialize relative_file_path
+      @file = File.read(relative_file_path)
+    end
+
+    def to_matrix
+      matrix = @file.each_line.collect{|line| SedokuLine.new(line).to_array}
+      return matrix.reject! {|a| a.nil?}
+    end
+
+
   end
 
-  def parse
-    matrix = []
+  class Line
+    def initialize line
+      @line =  line
+    end
+
+    def to_array
+      convert_to_array unless invalid?
+    end
+
+    private
+
+    def invalid?
+      @line[0] == '-'
+    end
+
+    def convert_to_array
+      @line.each_char.reject{|char| bad_characters.include? char}
+    end
+
+    def bad_characters
+      ['|', ' ', "\n"]
+    end
+
   end
 
+  class Array
+    def initialize line_array
+      @line_array = line_array
+    end
 
 
-
+  end
 end
--- test/sudoku_validator_test.rb
+++ test/sudoku_validator_test.rb
@@ -1,12 +1,28 @@
-require '../sudoku_validator'
+require_relative '../sudoku_validator'
 require 'minitest/autorun'
 
-class TestFileReading < Minitest::Test
+class TestSedokuToArray < Minitest::Test
 
-  def test_reads_a_file
-    assert true
+end
+
+class TestSedokuLine < Minitest::Test
+
+  def test_accepts_valid_line
+    assert Sedoku::Line.new("5 9 8 |7 3 6 |2 4 1 ").to_array
+  end
+
+  def test_rejects_invalid_line
+    assert_nil Sedoku::Line.new('-').to_array
+  end
+
+  def test_converts_valid_line_to_array
+    assert_equal ['5', '3', '2'], Sedoku::Line.new("5 |3 |2 ").to_array
   end
 
 end
+
+
+
+
 
 
